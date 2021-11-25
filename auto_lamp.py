d164b2c1f61205965484e208d6c8fc2de4af1e34
MZandtheRaspberryPi <ziegltrum@gmail.com>
2021-11-25 22:56:37 +0100

    cleaning up script

--- auto_lamp.py
+++ auto_lamp.py
@@ -8,163 +8,130 @@ import time
 import unicornhat as uh
 import snow
 
+
+
+LIGHT_YELLOW_3 = [255, 255, 102]
+BLUE_VIOLET = [138, 43, 226]
+
+
+# first wake up time, second wake up time, third wakeup time, fourth wakeup time
+# gone time, restart time, first bed time, second bed time, third bed time, offtime
+weekdayScheduleHours = [640, 650, 700, 730, 800, 1600, 2100, 2110, 2120, 2130]
+
+weekendScheduleHours = [800, 815, 830, 1000, 2200, 2220, 2230, 2240]
+
+def getClockTime():
+    dt = datetime.datetime.now()
+    clockTime = dt.strftime('%H%M')
+    clockTime = int(clockTime)
+    return clockTime
+
+def getDay():
+    dt = datetime.datetime.now()
+    day = dt.strftime('%A')
+    return day
+
 def sleepTillMorn(startTime):
-    while True:
-        dt = datetime.datetime.now()
-        clockTime = dt.strftime('%H%M')
-        clockTime = int(clockTime)
-        if clockTime >=startTime:
-            break
+    priorDay = getDay()
+    while getClockTime() < startTime or priorDay == getDay():
         time.sleep(10)
 
-def pulse(startTime,endTime,r,g,b,maxBrightness=.5,pulseTimeInterval=.01,brightStepInterval=1):
-    brightRange = list(range(200,int(maxBrightness*1000),brightStepInterval))
-    while True:
-        dt = datetime.datetime.now()
-        clockTime = dt.strftime('%H%M')
-        clockTime = int(clockTime)
-        logging.info(clockTime)
-        if clockTime >= startTime and clockTime < endTime:
-            for brightness in brightRange:
-                setAllPixels(r,g,b,brightness/1000)
-                time.sleep(pulseTimeInterval)
-            brightRange.reverse()
-            for brightness in brightRange:
-                setAllPixels(r,g,b,brightness/1000)
-                time.sleep(pulseTimeInterval)
-            brightRange.reverse()
-        else:
-            break
+def pulse(endTime, r, g, b, maxBrightness=.5, pulseTimeInterval=.01, brightStepInterval=1):
+    brightRange = list(range(200, int(maxBrightness*1000), brightStepInterval))
+    while getClockTime() < endTime:
+        for brightness in brightRange:
+            setAllPixels(r, g, b, brightness / 1000)
+            time.sleep(pulseTimeInterval)
+        brightRange.reverse()
+        for brightness in brightRange:
+            setAllPixels(r, g, b, brightness / 1000)
+            time.sleep(pulseTimeInterval)
+        brightRange.reverse()
 
-def setAllPixels(r,g,b,brightness):
+
+def setAllPixels(end_time, r, g, b, brightness):
     uh.brightness(brightness)
     for x in range(8):
         for y in range(8):
-            uh.set_pixel(x,y,r,g,b)
+            uh.set_pixel(x, y, r, g, b)
     uh.show()
+    while getClockTime() < end_time:
+        time.sleep(3)
 
-weekdaySchedule = {'firstWakeUpTime':640,
-'secondWakeUpTime':650,
-'thirdWakeUpTime':700,
-'fourthWakeUpTime':730,
-'goneTime':800,
-'restartTime':1600,
-'firstBedTime':2100,
-'secondBedTime':2110,
-'thirdBedTime':2120,
-'offTime':2130}
 
-weekendSchedule = {'firstWakeUpTime':800,
-'secondWakeUpTime':815,
-'thirdWakeUpTime':830,
-'fourthWakeUpTime':1000,
-'firstBedTime':2200,
-'secondBedTime':2220,
-'thirdBedTime':2230,
-'offTime':2240}
+def snow_wrapper(end_time, r, g, b, brightness):
+    snow.snow(end_time)
+
+def sleep_till_morn_wrapper(end_time, r, g, b, brightness):
+    sleepTillMorn(end_time)
+
+
+weekdaySchedule = {640: {"color": LIGHT_YELLOW_3, "brightness": .2, "function": setAllPixels},
+                   650: {"color": LIGHT_YELLOW_3, "brightness": .3, "function": setAllPixels},
+                   700: {"color": LIGHT_YELLOW_3, "brightness": .5, "function": pulse},
+                   730: {"color": LIGHT_YELLOW_3, "brightness": .5, "function": pulse},
+                   800: {"color": LIGHT_YELLOW_3, "brightness": .2, "function": sleep_till_morn_wrapper},
+                   1600: {"color": LIGHT_YELLOW_3, "brightness": .2, "function": snow_wrapper},
+                   2100: {"color": BLUE_VIOLET, "brightness": .6, "function": pulse},
+                   2110: {"color": BLUE_VIOLET, "brightness": .5, "function": pulse},
+                   2120: {"color": BLUE_VIOLET, "brightness": .4, "function": pulse},
+                   2130: {"color": BLUE_VIOLET, "brightness": .2, "function": sleep_till_morn_wrapper}}
+
+weekendSchedule = {800: {"color": LIGHT_YELLOW_3, "brightness": .2, "function": setAllPixels},
+                   815: {"color": LIGHT_YELLOW_3, "brightness": .3, "function": setAllPixels},
+                   830: {"color": LIGHT_YELLOW_3, "brightness": .5, "function": pulse},
+                   1000: {"color": LIGHT_YELLOW_3, "brightness": .2, "function": snow_wrapper},
+                   2200: {"color": BLUE_VIOLET, "brightness": .6, "function": pulse},
+                   2220: {"color": BLUE_VIOLET, "brightness": .5, "function": pulse},
+                   2230: {"color": BLUE_VIOLET, "brightness": .4, "function": pulse},
+                   2240: {"color": BLUE_VIOLET, "brightness": .2, "function": sleep_till_morn_wrapper}}
+
 
 if __name__ == "__main__":
     # this won't be run when imported
-    logging.basicConfig(filename = '/home/mikey/logs/autoLampLog.txt',level=logging.DEBUG,
-         format=' %(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(filename = '/home/mikey/logs/autoLampLog.txt', level=logging.DEBUG,
+                        format=' %(asctime)s - %(levelname)s - %(message)s')
     logging.disable(logging.DEBUG)
-    print('running lamp...')
+    logging.info('running lamp...')
 
     uh.set_layout(uh.PHAT)
 
     while True:
-        dt = datetime.datetime.now()
-        day = dt.strftime('%A')
-        clockTime =  dt.strftime('%H%M')
-        clockTime = int(clockTime)
-        lightYellow3 = [255,255,102]
-        blueViolet = [138,43,226]
-        #Weekend Section
-        if day in ['Saturday','Sunday']:
-            #logging.info("It's a weekend!")
-            if clockTime >= weekendSchedule['firstWakeUpTime'] and  clockTime <  weekendSchedule['secondWakeUpTime']:
-                r,g,b = lightYellow3[0],lightYellow3[1],lightYellow3[2]
-                setAllPixels(r,g,b,brightness=.2)
-            elif clockTime >= weekendSchedule['secondWakeUpTime'] and clockTime < weekendSchedule['thirdWakeUpTime']:
-                r,g,b = lightYellow3[0],lightYellow3[1],lightYellow3[2]
-                setAllPixels(r,g,b,brightness=.3)
-            elif clockTime >= weekendSchedule['thirdWakeUpTime'] and clockTime < weekendSchedule['fourthWakeUpTime']:
-                r,g,b = lightYellow3[0],lightYellow3[1],lightYellow3[2]
-                pulse(weekendSchedule['thirdWakeUpTime'],weekendSchedule['fourthWakeUpTime'],r,g,b)
-            elif day == 'Saturday' and clockTime >= weekendSchedule['fourthWakeUpTime'] and clockTime < weekendSchedule['firstBedTime']:
-                snow.snow(weekendSchedule['firstBedTime'])
-            elif day == 'Saturday' and clockTime >= weekendSchedule['firstBedTime'] and clockTime < weekendSchedule['secondBedTime']:
-                r,g,b = blueViolet[0],blueViolet[1],blueViolet[2]
-                pulse(weekendSchedule['firstBedTime'], weekendSchedule['secondBedTime'], r,g,b,maxBrightness = .6)
-            elif day == 'Saturday' and  clockTime >= weekendSchedule['secondBedTime'] and clockTime < weekendSchedule['thirdBedTime']:
-                r,g,b = blueViolet[0],blueViolet[1],blueViolet[2]
-                pulse(weekendSchedule['firstBedTime'],weekendSchedule['secondBedTime'],r,g,b,maxBrightness = .4)
-            elif day == 'Saturday' and  clockTime >= weekendSchedule['thirdBedTime'] and clockTime < weekendSchedule['offTime']:
-                r,g,b = blueViolet[0],blueViolet[1],blueViolet[2]
-                setAllPixels(r,g,b,brightness = .2)
-            elif day == 'Sunday' and clockTime >= weekendSchedule['fourthWakeUpTime'] and clockTime < weekdaySchedule['firstBedTime']:
-                snow.snow(weekdaySchedule['firstBedTime'])
-            elif day == 'Sunday' and clockTime >= weekdaySchedule['firstBedTime'] and clockTime < weekdaySchedule['secondBedTime']:
-                r,g,b = blueViolet[0],blueViolet[1],blueViolet[2]
-                pulse(weekdaySchedule['firstBedTime'], weekdaySchedule['secondBedTime'], r,g,b,maxBrightness = .5)
-            elif day == 'Sunday' and clockTime >= weekdaySchedule['secondBedTime'] and clockTime < weekdaySchedule['thirdBedTime']:
-                r,g,b = blueViolet[0],blueViolet[1],blueViolet[2]
-                pulse(weekdaySchedule['secondBedTime'],weekdaySchedule['thirdBedTime'],r,g,b,maxBrightness = .4)
-            elif day == 'Sunday' and clockTime >= weekdaySchedule['thirdBedTime'] and clockTime < weekdaySchedule['offTime']:
-                r,g,b = blueViolet[0],blueViolet[1],blueViolet[2]
-                setAllPixels(r,g,b,brightness = .2)
-            elif day == 'Saturday' and clockTime >= weekendSchedule['offTime']:
-                uh.clear()
-                uh.show()
-                logging.info("Sleeping till "+str(weekendSchedule['firstWakeUpTime']))
-                sleepTillMorn(weekendSchedule['firstWakeUpTime'])
-            elif day == 'Sunday' and clockTime >= weekdaySchedule['offTime']:
-                uh.clear()
-                uh.show()
-                logging.info("Sleeping till "+str(weekdaySchedule['firstWakeUpTime']))
-                sleepTillMorn(weekdaySchedule['firstWakeUpTime'])
-        #Weekday Section
-        else:
-            #logging.info("It's a weekday")
-            if clockTime >= weekdaySchedule['firstWakeUpTime'] and  clockTime <  weekdaySchedule['secondWakeUpTime']:
-                r,g,b = lightYellow3[0],lightYellow3[1],lightYellow3[2]
-                setAllPixels(r,g,b,brightness=.2)
-            elif clockTime >= weekdaySchedule['secondWakeUpTime'] and clockTime < weekdaySchedule['thirdWakeUpTime']:
-                r,g,b = lightYellow3[0],lightYellow3[1],lightYellow3[2]
-                setAllPixels(r,g,b,brightness=.3)
-            elif clockTime >= weekdaySchedule['thirdWakeUpTime'] and clockTime < weekdaySchedule['fourthWakeUpTime']:
-                r,g,b = lightYellow3[0],lightYellow3[1],lightYellow3[2]
-                pulse(weekdaySchedule['thirdWakeUpTime'],weekdaySchedule['fourthWakeUpTime'],r,g,b)
-            elif clockTime >= weekdaySchedule['fourthWakeUpTime'] and clockTime < weekdaySchedule['goneTime']:
-                r,g,b = lightYellow3[0],lightYellow3[1],lightYellow3[2]
-                pulse(weekdaySchedule['fourthWakeUpTime'],weekdaySchedule['fourthWakeUpTime'],r,g,b)
-            elif clockTime >= weekdaySchedule['goneTime'] and clockTime < weekdaySchedule['restartTime']:
-                uh.clear()
-                uh.show()
-                sleepTillMorn(weekdaySchedule['restartTime'])
-            elif clockTime >= weekdaySchedule['restartTime'] and clockTime < weekdaySchedule['firstBedTime']:
-                r,g,b = blueViolet[0],blueViolet[1],blueViolet[2]
-                pulse(weekdaySchedule['fourthWakeUpTime'],weekdaySchedule['firstBedTime'],r,g,b,maxBrightness=.6)
-            elif clockTime >= weekdaySchedule['firstBedTime'] and clockTime < weekdaySchedule['secondBedTime']:
-                r,g,b = blueViolet[0],blueViolet[1],blueViolet[2]
-                pulse(weekdaySchedule['firstBedTime'], weekdaySchedule['secondBedTime'], r,g,b,maxBrightness = .5)
-            elif clockTime >= weekdaySchedule['secondBedTime'] and clockTime < weekdaySchedule['thirdBedTime']:
-                r,g,b = blueViolet[0],blueViolet[1],blueViolet[2]
-                pulse(weekdaySchedule['firstBedTime'],weekdaySchedule['secondBedTime'],r,g,b,maxBrightness = .4)
-            elif clockTime >= weekdaySchedule['thirdBedTime'] and clockTime < weekdaySchedule['offTime'] and not day == 'Friday':
-                r,g,b = blueViolet[0],blueViolet[1],blueViolet[2]
-                setAllPixels(r,g,b,brightness = .2)
-            elif clockTime >= weekdaySchedule['offTime'] and not day == 'Friday':
-                uh.clear()
-                uh.show()
-                logging.info("Sleeping till "+str(weekdaySchedule['firstWakeUpTime']))
-                sleepTillMorn(weekdaySchedule['firstWakeUpTime'])
-            elif day == 'Friday' and clockTime >= weekdaySchedule['offTime'] and clockTime < weekendSchedule['offTime']:
-                r,g,b = blueViolet[0],blueViolet[1],blueViolet[2]
-                pulse(weekdaySchedule['offtime'],weekendSchedulej['offTime'],r,g,b,maxBrightness = .4)
-            elif day == 'Friday' and clockTime >= weekendSchedule['offTime']:
-                uh.clear()
-                uh.show()
-                logging.info("Sleeping till "+str(weekendSchedule['firstWakeUpTime']))
-                sleepTillMorn(weekendSchedule['firstWakeUpTime'])
-        time.sleep(10)
+        clockTime = getClockTime()
+        day = getDay()
+
+        settings_for_day = weekendSchedule if day == "Sunday" or day == "Saturday" else weekdaySchedule
+        hours_for_day = weekendScheduleHours if day == "Sunday" or day == "Saturday" else weekdayScheduleHours
+
+        current_hour_key = None
+        end_time = None
+        for i in range(0, len(hours_for_day)):
+            hour_min = hours_for_day[i]
+            if clockTime >= hour_min:
+                continue
+            elif not clockTime < hour_min:
+                current_hour_key = hours_for_day[i - 1]
+                end_time = hours_for_day[i]
+
+        # if we are past the last time, its turn off time
+        if current_hour_key is None and day not in ["Sunday", "Friday"]:
+            current_hour_key = hours_for_day[-1]
+            end_time = hours_for_day[0]
+        # monday sleep till weekday wakeup
+        elif current_hour_key is None and day == "Sunday":
+            current_hour_key = hours_for_day[-1]
+            end_time = weekdayScheduleHours[0]
+        elif current_hour_key is None and day == "Friday":
+            current_hour_key = hours_for_day[-1]
+            end_time = weekendScheduleHours[0]
+
+        logging.info("{} and current hour key is {} and end time is {}", day, current_hour_key, end_time)
+
+        current_hour_settings = settings_for_day[current_hour_key]
+        r, g, b = current_hour_settings["color"]
+        brightness = current_hour_settings["brightness"]
+        uh.clear()
+        uh.show()
+        current_hour_settings["function"](end_time, r, g, b, brightness)
+
+        time.sleep(2)
